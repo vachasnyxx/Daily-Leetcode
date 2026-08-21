8452ceaa0e8971cbb272d461acf938d865ecbb18
Vachaspati Tiwari <waynyxx@gmail.com>
2026-08-21 22:58:52 +0530

    Time: 0 ms (100%), Space: 8.1 MB (37.54%) - LeetHub

---
+++ 1802-maximum-value-at-a-given-index-in-a-bounded-array/1802-maximum-value-at-a-given-index-in-a-bounded-array.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    typedef long long ll;
+    ll getSumByFormula(ll count, ll x) {
+        return (count * x) - (count * (count + 1) / 2);
+    }
+
+    int maxValue(int n, int index, int maxSum) {
+        ll left = 1;
+        ll right = maxSum;
+        ll mid_val;
+        int result = 0;
+
+        while (left <= right) {
+            mid_val = left + (right - left) / 2;
+            // -----------------------Left-----------
+            ll left_count = min((ll)index, mid_val - 1);
+            ll left_sum = getSumByFormula(left_count, mid_val);
+            left_sum += max((ll)0, index - (mid_val - 1)); // extra ones if any
+
+            // -----------------------Right-----------
+            ll right_count = min((ll)n - index - 1, mid_val - 1);
+            ll right_sum = getSumByFormula(right_count, mid_val);
+            right_sum += max((ll)0, n-index-1 - (mid_val - 1)); // extra ones if any
+
+            // -----------------------Calculation-----------
+
+            ll total_sum = left_sum + mid_val + right_sum;
+
+            if (total_sum <= maxSum) {
+                result = max((ll)result, mid_val);
+                left = mid_val + 1;
+            } else {
+                right = mid_val - 1;
+            }
+        }
+        return result;
+    }
+};

63ba96b487525ad997e322c53f0e2590ea8797b6
Vachaspati Tiwari <waynyxx@gmail.com>
2026-08-31 04:14:46 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (35.26%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -2,34 +2,65 @@ class Solution {
 public:
     int n;
     vector<vector<int>> result;
-    unordered_set<int> st;
 
-    void solve(vector<int>& temp,vector<int>& nums){
-        if(temp.size() == n){
-            result.push_back(temp);
+    void solve(int idx, vector<int>& nums) {
+        if (idx == n) {
+            result.push_back(nums);
             return;
         }
 
-        for(int i = 0; i<n ; i++){
+        for (int i = idx; i < n; i++) {
 
-            if(st.find(nums[i]) == st.end()){
+            swap(nums[i], nums[idx]);
 
-                temp.push_back(nums[i]);
-                st.insert(nums[i]);
+            solve(idx + 1, nums);
 
-                solve(temp,nums);
-
-                temp.pop_back();
-                st.erase(nums[i]);
-            }
+            swap(nums[i], nums[idx]);
         }
     }
 
     vector<vector<int>> permute(vector<int>& nums) {
-        n  = nums.size();
-        vector<int> temp;
-        solve(temp,nums);
+        n = nums.size();
+
+        solve(0, nums);
 
         return result;
     }
 };
+
+// Approach 1
+// class Solution {
+// public:
+//     int n;
+//     vector<vector<int>> result;
+//     unordered_set<int> st;
+
+//     void solve(vector<int>& temp,vector<int>& nums){
+//         if(temp.size() == n){
+//             result.push_back(temp);
+//             return;
+//         }
+
+//         for(int i = 0; i<n ; i++){
+
+//             if(st.find(nums[i]) == st.end()){
+
+//                 temp.push_back(nums[i]);
+//                 st.insert(nums[i]);
+
+//                 solve(temp,nums);
+
+//                 temp.pop_back();
+//                 st.erase(nums[i]);
+//             }
+//         }
+//     }
+
+//     vector<vector<int>> permute(vector<int>& nums) {
+//         n  = nums.size();
+//         vector<int> temp;
+//         solve(temp,nums);
+
+//         return result;
+//     }
+// };

57857e6bfe3c24abb72afb3dab15af5392fb3851
Vachaspati Tiwari <waynyxx@gmail.com>
2026-09-02 20:29:00 +0530

    Time: 19 ms (72.63%), Space: 8 MB (84.57%) - LeetHub

--- 2698-find-the-punishment-number-of-an-integer/2698-find-the-punishment-number-of-an-integer.cpp
+++ 2698-find-the-punishment-number-of-an-integer/2698-find-the-punishment-number-of-an-integer.cpp
@@ -1,46 +1,23 @@
+
 class Solution {
 public:
-    bool check(int i, int currsum, string s, int num, vector<vector<int>>& t){
-        if(i == s.size()){
+    bool check(int sq, int currsum, int num) {
+
+        if (sq == 0) {
             return currsum == num;
         }
 
-        if(currsum > num){
-            return false;
-        }
-
-        if(t[i][currsum] != -1){
-            return t[i][currsum];
-        }
-
-        bool possible = false;
-
-        for(int j = i; j<s.size(); j++){
-            string sub = s.substr(i, j-i+1);
-
-            int val = stoi(sub);
-
-            possible = possible || check(j+1, currsum+val,s,num,t);
-
-            if(possible == true){
-                return true;
-            }
-        }
-
-        return  t[i][currsum] = possible;
+        return check(sq / 10, currsum + sq % 10, num) ||
+               check(sq / 100, currsum + sq % 100, num) ||
+               check(sq / 1000, currsum + sq % 1000, num) || check(sq / 10000, currsum + sq % 10000, num);
     }
     int punishmentNumber(int n) {
         int punish = 0;
-        
 
-        for(int num  = 1; num <= n; num++){
+        for (int num = 1; num <= n; num++) {
             int sq = num * num;
 
-            string s = to_string(sq);
-
-            vector<vector<int>> t(s.length(), vector<int> (num+1, -1));
-
-            if(check(0,0,s,num,t) == true){
+            if (check(sq, 0, num) == true) {
                 punish += sq;
             }
         }
@@ -48,3 +25,74 @@ public:
         return punish;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     bool check(int i, int currsum, string s, int num, vector<vector<int>>&
+//     t){
+//         if(i == s.size()){
+//             return currsum == num;
+//         }
+
+//         if(currsum > num){
+//             return false;
+//         }
+
+//         if(t[i][currsum] != -1){
+//             return t[i][currsum];
+//         }
+
+//         bool possible = false;
+
+//         for(int j = i; j<s.size(); j++){
+//             string sub = s.substr(i, j-i+1);
+
+//             int val = stoi(sub);
+
+//             possible = possible || check(j+1, currsum+val,s,num,t);
+
+//             if(possible == true){
+//                 return true;
+//             }
+//         }
+
+//         return  t[i][currsum] = possible;
+//     }
+//     int punishmentNumber(int n) {
+//         int punish = 0;
+
+//         for(int num  = 1; num <= n; num++){
+//             int sq = num * num;
+
+//             string s = to_string(sq);
+
+//             vector<vector<int>> t(s.length(), vector<int> (num+1, -1));
+
+//             if(check(0,0,s,num,t) == true){
+//                 punish += sq;
+//             }
+//         }
+
+//         return punish;
+//     }
+// };

7a43e2b609b99f4acb46439edf11e6d8cb0ecbe9
Vachaspati Tiwari <waynyxx@gmail.com>
2026-09-05 21:01:07 +0530

    Time: 0 ms (100.00%) | Memory: 31.1 MB (66.17%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
@@ -2,21 +2,22 @@ class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int> suffMin(n);
-        
-        // Step 1: Precompute suffix minimums
-        suffMin[n - 1] = nums[n - 1];
-        for (int i = n - 2; i >= 0; --i) {
-            suffMin[i] = min(suffMin[i + 1], nums[i]);
+
+        vector<int> minFromIndex(n);
+
+        int minEl = INT_MAX;
+
+        for (int i = n - 1; i >= 0; i--) {
+            minEl = min(minEl, nums[i]);
+            minFromIndex[i] = minEl;
         }
-        
-        // Step 2: Traverse with prefix maximum
-        int prefMax = nums[0];
-        for (int i = 0; i < n; ++i) {
-            prefMax = max(prefMax, nums[i]);
-            int instability = prefMax - suffMin[i];
-            if (instability <= k) {
-                return i;  // return smallest stable index
+
+        int maxEl = INT_MIN;
+
+        for (int i = 0; i < n; i++) {
+            maxEl = max(maxEl, nums[i]);
+            if (maxEl - minFromIndex[i] <= k) {
+                return i;
             }
         }
         return -1;

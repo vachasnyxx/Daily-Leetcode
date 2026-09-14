8f76a5eb0e271dd2e59dc4aa2ae882d17e9c2869
Vachaspati Tiwari <waynyxx@gmail.com>
2026-09-14 16:43:51 +0530

    Time: 0 ms (100%), Space: 9.9 MB (64.32%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -8,23 +8,72 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+// public:
+//     ListNode* middleNode(ListNode* head) {
+//         int len = 0;
+//         ListNode* temp = head;
+
+//         while(temp != NULL){
+//             len++;
+//             temp= temp->next;
+//         }
+
+//         int midIdx = len/2;
+//         ListNode* mid = head;
+//         for(int i = 1; i<= midIdx; i++){
+//             mid = mid->next;
+//         }
+//         return mid;
+
+//     }
+// };
+
+
+
+
+
+// class Solution {
+// public:
+//     ListNode* middleNode(ListNode* head) {
+//         ListNode* slow = head;
+//         ListNode* fast = head;
+
+//         while(fast != NULL && fast->next != NULL){
+//             slow = slow->next;
+//             fast = fast->next->next;
+//         }
+//         return slow;
+//     }
+// };
+
+
+
+
+
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        int len = 0;
-        ListNode* temp = head;
+        // Initialize two pointers:
+        // 'slow' moves one step at a time
+        // 'fast' moves two steps at a time
+        ListNode* slow = head;
+        ListNode* fast = head;
 
-        while(temp != NULL){
-            len++;
-            temp= temp->next;
+        // Loop continues as long as:
+        // 1. 'fast' is not NULL (list not fully traversed)
+        // 2. 'fast->next' is not NULL (so 'fast' can safely move two steps)
+        while(fast != NULL && fast->next != NULL){
+            // Move 'slow' one step forward
+            slow = slow->next;
+
+            // Move 'fast' two steps forward
+            fast = fast->next->next;
         }
 
-        int midIdx = len/2;
-        ListNode* mid = head;
-        for(int i = 1; i<= midIdx; i++){
-            mid = mid->next;
-        }
-        return mid;
-
+        // When loop ends:
+        // - 'fast' has reached the end (or gone past it)
+        // - 'slow' is exactly at the middle node
+        return slow;
     }
 };

267962bca4ee5c4144f7cd5c5908b086739f67ad
Vachaspati Tiwari <waynyxx@gmail.com>
2026-09-22 15:59:58 +0530

    Time: 4 ms (50.42%), Space: 23.3 MB (79.16%) - LeetHub

--- 0382-linked-list-random-node/0382-linked-list-random-node.cpp
+++ 0382-linked-list-random-node/0382-linked-list-random-node.cpp
@@ -1,16 +1,46 @@
+// class Solution {
+// public:
+//     vector<int> arr;
+//     Solution(ListNode* head){
+//         ListNode* temp = head;
+//         while(temp != NULL){
+//             arr.push_back(temp->val);
+//             temp = temp->next;
+//         }
+//     }
+//     int getRandom() {
+//         int n = arr.size();
+//         int random_index = rand()%n;   //{0, 1, ..... n- 1}
+//         return arr[random_index];
+//     }
+// };
+
+
+
+
+
+
 class Solution {
 public:
-    vector<int> arr;
+    ListNode* Head;
+
     Solution(ListNode* head){
-        ListNode* temp = head;
-        while(temp != NULL){
-            arr.push_back(temp->val);
-            temp = temp->next;
-        }
+        Head = head;
     }
     int getRandom() {
-        int n = arr.size();
-        int random_index = rand()%n;   //{0, 1, ..... n- 1}
-        return arr[random_index];
+        int count = 1;
+        int result = 0;
+
+        ListNode* temp = Head;
+
+        while(temp != NULL){
+            if(rand()% count <1.0/count){
+                result = temp->val;
+            }
+            count++;
+            temp = temp->next;
+        }
+        return result;
     }
 };
+

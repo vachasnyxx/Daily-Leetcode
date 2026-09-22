dbf7c0a18730c0f6a34b08d822b39b3b04d22432
Vachaspati Tiwari <waynyxx@gmail.com>
2026-09-22 17:42:48 +0530

    Time: 0 ms (100%), Space: 124.4 MB (48.56%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -1,34 +1,94 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     int pairSum(ListNode* head) {
-        vector<int> vec;
+        ListNode* slow = head;
+        ListNode* fast = head;
 
-        ListNode* curr = head;
-
-        while(curr != NULL){
-            vec.push_back(curr->val);
-            curr = curr->next;
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
         }
+
+        ListNode* mid = slow;
+
+        // rverse 2nd hafl of our Linked List
+        // reversal code
+
+        ListNode* prev = NULL;
+        ListNode* nextNode = NULL;
+
+        while (mid != NULL) {
+            nextNode = mid->next;
+            mid->next = prev;
+            prev = mid;
+            mid = nextNode;
+        }
+
+        // find max result
 
         int result = 0;
 
-        int i = 0, j = vec.size() - 1;
+        ListNode* curr = head;
 
-        while(i < j){
-            result = max(result,  vec[i] + vec[j]);
-            i++;
-            j--;
+        while (prev != NULL) {
+            result = max(result, curr->val + prev->val);
+            curr = curr->next;
+            prev = prev->next;
         }
         return result;
     }
 };
+
+// using stack
+// class Solution {
+// public:
+//     int pairSum(ListNode* head) {
+//         stack<int> st;
+
+//         ListNode* curr = head;
+
+//         while( curr != NULL){
+//             st.push(curr->val);
+//             curr = curr->next;
+//         }
+//         int N = st.size();
+//         curr = head;
+//         int count = 1;
+
+//         int result = 0;
+
+//         while(count <= N/2){
+//             result = max(result, curr->val + st.top());
+//             curr = curr->next;
+//             st.pop();
+//             count++;
+//         }
+//         return result;
+//     }
+// };
+
+// using vector
+// class Solution {
+// public:
+//     int pairSum(ListNode* head) {
+//         vector<int> vec;
+
+//         ListNode* curr = head;
+
+//         while(curr != NULL){
+//             vec.push_back(curr->val);
+//             curr = curr->next;
+//         }
+
+//         int result = 0;
+
+//         int i = 0, j = vec.size() - 1;
+
+//         while(i < j){
+//             result = max(result,  vec[i] + vec[j]);
+//             i++;
+//             j--;
+//         }
+//         return result;
+//     }
+// };
